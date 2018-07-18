0d32804730a2ea6e311e2eb5f96104b58fdf09ef
antonPetrovskiy <toschapetrovski@gmail.com>
2018-07-18 11:29:39 +0300

    copy referral

--- app/src/main/java/com/professor/traficinspiration/activity/ReferralsActivity.java
+++ app/src/main/java/com/professor/traficinspiration/activity/ReferralsActivity.java
@@ -1,20 +1,24 @@
 package com.professor.traficinspiration.activity;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.professor.traficinspiration.ApplicationContext;
 import com.professor.traficinspiration.R;
 
 public class ReferralsActivity extends AppCompatActivity implements View.OnClickListener {
 
+    TextView tv;
+    Context c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_referrals);
-
         findViewById(R.id.back_button).setOnClickListener(this);
         ((TextView)findViewById(R.id.header_title)).setText("Рефералы");
 
@@ -22,6 +26,15 @@ public class ReferralsActivity extends AppCompatActivity implements View.OnClick
         ((TextView) findViewById(R.id.text_referral_code)).setText(String.valueOf(ApplicationContext.getUser().getId()));
         ((TextView) findViewById(R.id.text_referral_income)).setText(String.valueOf(ApplicationContext.getUser().getReferralIncome()));
 
+        tv = findViewById(R.id.text_referral_code);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast toast = Toast.makeText(getApplicationContext(),
+                        "Скопировано", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 
     @Override
